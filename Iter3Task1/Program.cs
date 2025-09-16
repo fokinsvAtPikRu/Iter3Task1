2a83d293fc1a221ba46872de82f01e0924702bf1
Фокин Серей <fokinsv@pik.ru>
2025-09-16 07:28:19 +0400

    Добавьте файлы проекта.

---
+++ Iter3Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Iter3Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
