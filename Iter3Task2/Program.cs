37f0dd5043b26d1f7cdbd307e64ca68df7633410
Фокин Серей <fokinsv@pik.ru>
2025-10-03 16:08:20 +0300

    Итерация 3. Задача 2 готова.

--- Iter3Task2/Program.cs
+++ Iter3Task2/Program.cs
@@ -6,17 +6,37 @@ namespace Iter3Task2
         static void Main(string[] args)
         {
             ServiceCollection services = new ServiceCollection();
+            bool isChecked = false;
+            while (!isChecked)
+            {
+                Console.Clear();
+                Console.Write("Выберите способ уведомления\n1 - email\n2 - SMS\nВаш выбор:");
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    switch (result)
+                    {
+                        case 1:
+                            services.AddSingleton<INotificationSender, EmailSender>();
+                            isChecked = true;
+                            break;
+                        case 2:
+                            services.AddSingleton<INotificationSender, SmsSender>();
+                            isChecked = true;
+                            break;
+                    }
+                }
+            }
+
             
-            services.AddSingleton<INotificationSender,EmailSender>();
             services.AddSingleton<NotificationService>();
 
             services.AddSingleton<ILoger, FileLoger>();
             services.AddSingleton<LoginService>();
 
 
-            var provider=services.BuildServiceProvider();
-            
-            var service=provider.GetRequiredService<NotificationService>();
+            var provider = services.BuildServiceProvider();
+
+            var service = provider.GetRequiredService<NotificationService>();
             var loger = provider.GetRequiredService<LoginService>();
 
 
