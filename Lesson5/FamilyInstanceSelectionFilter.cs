23a169b5737f74ea7f849ab53ac56b42145d50c6
Фокин Серей <fokinsv@pik.ru>
2025-11-22 11:02:30 +0400

    Задание к 5 уроку

---
+++ Lesson5/FamilyInstanceSelectionFilter.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace Lesson5
+{
+    public class FamilyInstanceSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem) => elem is FamilyInstance;
+
+        public bool AllowReference(Reference reference, XYZ position) => false;
+        
+    }
+}
---
+++ Lesson5/StatisticSelectionElements.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson5
+{
+    [Transaction(TransactionMode.Manual)]
+    public class StatisticSelectionElements : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Application app = uiApp.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            IList<Reference> refs;
+            try 
+            {
+                    refs = uiDoc.Selection.PickObjects(ObjectType.Element,
+                    new FamilyInstanceSelectionFilter(),
+                    "Pick Objects");            
+            }
+            catch 
+            {
+                TaskDialog.Show("Result", "No Items Selected");
+                return Result.Cancelled;
+            }
+            Dictionary<string, int> categories = new Dictionary<string, int>();
+            var resultMessage = new StringBuilder($"Общее количество элементов - {refs.Count}\n");
+            foreach (Reference r in refs)
+            {
+                Element element =doc.GetElement(r);
+                string categoryName = element.Category.Name;
+                if (categories.ContainsKey(categoryName))
+                {
+                    categories[categoryName]++;
+                }
+                else
+                {
+                    categories.Add(categoryName, 1);
+                }
+            }
+            foreach (var category in categories)
+            {
+                resultMessage.Append($"{category.Key} - {category.Value}\n");
+            }
+            TaskDialog.Show("Итого", resultMessage.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
